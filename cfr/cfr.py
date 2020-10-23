1864c7c471441aa3b3aac4723bea22c135c45820
s.takeuchi <>
2020-10-23 11:26:00 +0900

    implement exploitability

--- cfr/cfr.py
+++ cfr/cfr.py
@@ -1,35 +1,37 @@
 from copy import copy
 from copy import deepcopy
-import yaml
 
 from tqdm import tqdm
+import yaml
 
+import logger
 from envs.toy_pokers import Node, KuhnPoker
 
 
-def update_pi(node: Node, policy: dict, pi_mi: list, pi_i: list):
-    """
-    update probability
-    """
-    node.pi = pi_mi[node.player] * pi_i[node.player]
-    node.pi_mi = pi_mi[node.player]
-    node.pi_i = pi_i[node.player]
+def update_pi(node: Node, strategy_profile: dict, average_strategy_profile: dict, pi_mi_list: list, pi_i_list: list, true_pi_mi_list: list):
+    node.pi = pi_mi_list[node.player] * pi_i_list[node.player]
+    node.true_pi_mi = true_pi_mi_list[node.player]
+    node.pi_mi = pi_mi_list[node.player]
+    node.pi_i = pi_i_list[node.player]
 
     if node.terminal:
         return
     for action, child_node in node.children.items():
-        next_pi_mi = copy(pi_mi)
-        next_pi_i = copy(pi_i)
-        for player in policy.keys():
+        next_pi_mi_list = copy(pi_mi_list)
+        next_pi_i_list = copy(pi_i_list)
+        next_true_pi_mi_list = copy(true_pi_mi_list)
+        for player in strategy_profile.keys():
             if player == node.player:
-                next_pi_i[player] *= policy[node.player][node.information][action]
+                next_pi_i_list[player] *= strategy_profile[node.player][node.information][action]
             else:
-                next_pi_mi[player] *= policy[node.player][node.information][action]
-        update_pi(child_node, policy, next_pi_mi, next_pi_i)
+                next_pi_mi_list[player] *= strategy_profile[node.player][node.information][action]
+                next_true_pi_mi_list[player] *= average_strategy_profile[node.player][node.information][action]
+        update_pi(child_node, strategy_profile, average_strategy_profile, next_pi_mi_list, next_pi_i_list, next_true_pi_mi_list)
 
 
 def update_node_values(node: Node, strategy_profile: dict):
     node_eu = 0  # ノードに到達した後得られる期待利得
+    node.num_updates += 1
     if node.terminal:
         return node.eu
     node.pi_i_sum += node.pi_i
@@ -45,7 +47,7 @@ def update_node_values(node: Node, strategy_profile: dict):
     return node_eu
 
 
-def get_initial_strategy(node: Node, num_players=None, strategy_profile=None):
+def get_initial_strategy_profile(node: Node, num_players=None, strategy_profile=None):
     if node.terminal:
         return strategy_profile
     if strategy_profile is None:
@@ -53,7 +55,7 @@ def get_initial_strategy(node: Node, num_players=None, strategy_profile=None):
     if node.information not in strategy_profile[node.player]:
         strategy_profile[node.player][node.information] = {action: 1 / len(node.children) for action in node.children}
     for child in node.children.values():
-        strategy_profile = get_initial_strategy(child, strategy_profile=strategy_profile)
+        strategy_profile = get_initial_strategy_profile(child, strategy_profile=strategy_profile)
     return strategy_profile
 
 
@@ -82,42 +84,107 @@ def update_strategy(strategy_profile: dict, average_strategy_profile: dict, info
     return
 
 
-def train(num_iter):
-    kuhn_poker = KuhnPoker()
-    strategy_profile = get_initial_strategy(kuhn_poker.root, kuhn_poker.num_players)
+def compute_exploitability(node, information_sets, average_strategy_profile, opponent_player):
+    """
+    exploitability when opponent player chooses best response for current strategy
+    """
+    if node.terminal:
+        return node.eu
+    if node.player == opponent_player:  # choose exploitable strategy
+        best_response_value = None
+        best_weighted_expected_utility = -float("inf") if opponent_player == 0 else float("inf")  # current best value
+        for action, _ in node.children.items():
+            current_weighted_expected_utility = 0  # current value of sum of reach_p * expected_utility
+            current_node_expected_utility = 0
+            for same_info_node in information_sets[node.player][node.information]:
+                expected_utility = compute_exploitability(same_info_node.children[action], information_sets, average_strategy_profile, opponent_player)
+                if node == same_info_node:
+                    current_node_expected_utility = expected_utility
+                current_weighted_expected_utility += same_info_node.true_pi_mi * expected_utility
+            if opponent_player == 0:
+                if best_weighted_expected_utility < current_weighted_expected_utility:
+                    best_weighted_expected_utility = current_weighted_expected_utility
+                    best_response_value = current_node_expected_utility
+            else:
+                if best_weighted_expected_utility > current_weighted_expected_utility:
+                    best_weighted_expected_utility = current_weighted_expected_utility
+                    best_response_value = current_node_expected_utility
+        return best_response_value
+
+    else:  # use current strategy
+        expected_utility = 0
+        for action, child_node in node.children.items():
+            p = average_strategy_profile[node.player][node.information][action]
+            expected_utility += p * compute_exploitability(child_node, information_sets, average_strategy_profile, opponent_player)
+    return expected_utility
+
+
+def get_exploitability(game, average_strategy_profile):
+    exploitability = 0
+    for player in range(game.num_players):
+        if player == 0:
+            exploitability += compute_exploitability(game.root, game.information_sets, average_strategy_profile, player)
+        else:
+            exploitability += (-1) * compute_exploitability(game.root, game.information_sets, average_strategy_profile, player)
+    return exploitability
+
+
+def check_exploitability():
+    """
+    KuhnPoker
+    """
+    game = KuhnPoker()
+    nash_equilibrium = game.get_nash_equilibrium(game.root)
+    update_pi(game.root, nash_equilibrium, nash_equilibrium, [1.0 for _ in range(game.num_players + 1)],
+              [1.0 for _ in range(game.num_players + 1)], [1.0 for _ in range(game.num_players + 1)])
+    exploitability = get_exploitability(game, nash_equilibrium)
+    print(exploitability)
+
+
+def train(num_iter, log_schedule):
+    game = KuhnPoker()
+    strategy_profile = get_initial_strategy_profile(game.root, game.num_players)
     average_strategy_profile = deepcopy(strategy_profile)
-
-    for _ in tqdm(range(num_iter)):
-        update_pi(kuhn_poker.root, strategy_profile, [1.0 for _ in range(kuhn_poker.num_players + 1)], [1.0 for _ in range(kuhn_poker.num_players + 1)])
-        update_node_values(kuhn_poker.root, strategy_profile)
-        update_strategy(strategy_profile, average_strategy_profile, kuhn_poker.information_sets)
-
+    for t in tqdm(range(num_iter)):
+        update_pi(game.root, strategy_profile, average_strategy_profile, [1.0 for _ in range(game.num_players + 1)], [1.0 for _ in range(game.num_players + 1)], [1.0 for _ in range(game.num_players + 1)])
+        update_node_values(game.root, strategy_profile)
+        exploitability = get_exploitability(game, average_strategy_profile)
+        update_strategy(strategy_profile, average_strategy_profile, game.information_sets)
+        if t % log_schedule(t) == 0:
+            logger.logkv("t", t)
+            logger.logkv("exploitability", exploitability)
+            logger.dumpkvs()
     return average_strategy_profile
 
 
-def main():
-    average_strategy_profile = train(1000000)
+def add_dict_to_dict(d: dict, key):
+    if key not in d:
+        d[key] = {}
+
+
+def export_strategy_profile_to_yaml(strategy_profile_result):
     result = {}
-    for player, sigma in average_strategy_profile.items():
+    for player, sigma in strategy_profile_result.items():
         if player == -1:
             continue
         for info, p_dist in sigma.items():
-            add_dtd(result, info[0][0])
+            add_dict_to_dict(result, info[0][0])
             if len(info[1]) == 0:
                 history = "-"
             else:
                 history = "-".join(info[1])
             for action, p in p_dist.items():
-                add_dtd(result[info[0][0]], history)
+                add_dict_to_dict(result[info[0][0]], history)
                 result[info[0][0]][history][action] = p
     with open("../sample_result.yaml", "w") as f:
         yaml.dump(result, f)
-    return
 
 
-def add_dtd(d: dict, key):
-    if key not in d:
-        d[key] = {}
+def main():
+    logger.configure("./logs")
+    num_updates = int(5e7)
+    average_strategy_profile = train(num_updates, lambda x: (10 ** (len(str(x)) - 1)))
+    export_strategy_profile_to_yaml(average_strategy_profile)
 
 
 if __name__ == "__main__":
--- cfr/envs/toy_pokers.py
+++ cfr/envs/toy_pokers.py
@@ -9,6 +9,37 @@ def add_list_to_dict(target_dict, key, value):
         target_dict[key] = [value]
 
 
+class Node:
+    def __init__(self, player, terminal, eu=0):
+        self.children = {}
+        self.player = player
+        self.terminal = terminal
+        self.private_cards = []
+        self.history = []
+        self.information = ((), ())  # (private card, history)
+
+        self.pi = 0
+        self.pi_mi = 0  # pi_-i
+        self.pi_i = 0  # pi_i
+        self.true_pi_mi = 0  # pi_-i following current average strategy profile
+        self.eu = eu
+        self.cv = 0
+        self.cfr = {}  # counter-factual regret of not taking action a at history h(not information I)
+        self.pi_i_sum = 0  # denominator of average strategy
+        self.pi_sigma_sum = {}  # numerator of average strategy
+        self.num_updates = 0
+
+    def expand_child_node(self, action, next_player, terminal, utility=0, private_cards=None):
+        next_node = Node(next_player, terminal, utility)
+        self.children[action] = next_node
+        self.cfr[action] = 0
+        self.pi_sigma_sum[action] = 0
+        next_node.private_cards = self.private_cards if private_cards is None else private_cards
+        next_node.history = self.history + [action] if self.player != -1 else self.history
+        next_node.information = (next_node.private_cards[next_player], tuple(next_node.history))
+        return next_node
+
+
 class Card:
     def __init__(self, rank, suit=None):
         self.rank = rank
@@ -19,35 +50,6 @@ class Card:
             return str(self.rank)
         else:
             return str(self.rank) + str(self.suit)
-
-
-class Node:
-    def __init__(self, player: int, terminal: bool, eu: float = 0):
-        self.children = {}
-        self.player = player
-        self.terminal = terminal
-        self.private_cards = []
-        self.history = []
-        self.information = ()  # (private card, history)
-
-        self.pi = 0
-        self.pi_mi = 0  # pi_-i
-        self.pi_i = 0  # pi_i
-        self.eu = eu
-        self.cv = 0
-        self.cfr = {}  # counterfactual regret of not taking action a at history h(not information I)
-        self.pi_i_sum = 0  # denominator of average strategy
-        self.pi_sigma_sum = {}  # numerator of average strategy
-
-    def expand_child_node(self, action: str, next_player: int, terminal: bool, utility: float = 0, private_cards=None):
-        next_node = Node(next_player, terminal, utility)
-        self.children[action] = next_node
-        self.cfr[action] = 0
-        self.pi_sigma_sum[action] = 0
-        next_node.private_cards = self.private_cards if private_cards is None else private_cards
-        next_node.history = self.history + [action] if self.player != -1 else self.history
-        next_node.information = (next_node.private_cards[next_player], tuple(next_node.history))
-        return next_node
 
 
 class KuhnPoker:
@@ -71,39 +73,110 @@ class KuhnPoker:
                 node = root.expand_child_node(str(*hand_0) + ',' + str(*hand_1), next_player, False, private_cards=private_cards)
                 add_list_to_dict(self.information_sets[next_player], node.information, node)
                 stack.append(node)
-                for action in ["check", "bet"]:  # player 0 actions
+                for action in ['check', 'bet']:  # player 0 actions
                     next_player = 1
                     node = node.expand_child_node(action, next_player, False)
                     add_list_to_dict(self.information_sets[next_player], node.information, node)
                     stack.append(node)
-                    if action == "check":
-                        for action in ["check", "bet"]:  # player 1 actions
-                            if action == "check":
+                    if action == 'check':
+                        for action in ['check', 'bet']:  # player 1 actions
+                            if action == 'check':
                                 utility = self._compute_utility(action, next_player, hand_0, hand_1)
                                 next_player = -1
                                 node = node.expand_child_node(action, next_player, True, utility)
                                 add_list_to_dict(self.information_sets[next_player], node.information, node)
                                 node = stack.pop()
-                            if action == "bet":
+                            if action == 'bet':
                                 next_player = 0
                                 node = node.expand_child_node(action, next_player, False)
                                 add_list_to_dict(self.information_sets[next_player], node.information, node)
                                 stack.append(node)
-                                for action in ["fold", "call"]:  # player 0 actions
+                                for action in ['fold', 'call']:  # player 0 actions
                                     utility = self._compute_utility(action, next_player, hand_0, hand_1)
                                     next_player = -1
                                     node = node.expand_child_node(action, next_player, True, utility)
                                     add_list_to_dict(self.information_sets[next_player], node.information, node)
                                     node = stack.pop()
-                    if action == "bet":
+                    if action == 'bet':
                         stack.append(node)
-                        for action in ["fold", "call"]:  # player 1 actions
+                        for action in ['fold', 'call']:  # player 1 actions
                             utility = self._compute_utility(action, next_player, hand_0, hand_1)
                             next_player = -1
                             node = node.expand_child_node(action, next_player, True, utility)
                             add_list_to_dict(self.information_sets[next_player], node.information, node)
                             node = stack.pop()
         return root
+
+    def get_nash_equilibrium(self, node: Node, strategy_profile=None):
+        """
+        nash equilibrium (alpha = 0)
+        """
+        if node.terminal:
+            return strategy_profile
+        if strategy_profile is None:
+            strategy_profile = {player: {} for player in range(-1, self.num_players)}
+        if node.information not in strategy_profile[node.player]:
+            hand = node.information[0]
+            if len(hand) == 0:
+                strategy_profile[node.player][node.information] = {action: 1 / len(node.children) for action in node.children}
+            else:
+                card = hand[0]
+                for action, child in node.children.items():
+                    if node.player == 0:
+                        if action == "bet":
+                            p = 0
+                        elif action == "check":
+                            p = 1
+                        elif action == "call":
+                            if card == 0:
+                                p = 0
+                            elif card == 1:
+                                p = 1/3
+                            else:
+                                p = 1
+                        else:
+                            if card == 0:
+                                p = 1
+                            elif card == 1:
+                                p = 2/3
+                            else:
+                                p = 0
+                    else:
+                        if action == "bet":
+                            if card == 0:
+                                p = 1/3
+                            elif card == 1:
+                                p = 0
+                            else:
+                                p = 1
+                        elif action == "check":
+                            if card == 0:
+                                p = 2/3
+                            elif card == 1:
+                                p = 1
+                            else:
+                                p = 0
+                        elif action == "call":
+                            if card == 0:
+                                p = 0
+                            elif card == 1:
+                                p = 1/3
+                            else:
+                                p = 1
+                        else:
+                            if card == 0:
+                                p = 1
+                            elif card == 1:
+                                p = 2/3
+                            else:
+                                p = 0
+                    if node.information not in strategy_profile[node.player]:
+                        strategy_profile[node.player][node.information] = {}
+                    strategy_profile[node.player][node.information][action] = p
+        for child in node.children.values():
+            strategy_profile = self.get_nash_equilibrium(child, strategy_profile=strategy_profile)
+        return strategy_profile
+
 
     def _compute_utility(self, action, player, hand_0, hand_1):
         card_0, card_1 = hand_0[0], hand_1[0]
@@ -121,3 +194,4 @@ class KuhnPoker:
 
 if __name__ == "__main__":
     kuhn_poker = KuhnPoker()
+
